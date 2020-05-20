af6a83e4bdf0614bd60ffc5f33df73ac9fb12bcd
ajgreenwell <greenwan@bc.edu>
2020-05-20 15:49:41 -0400

    Updates

---
+++ precache-manifest.15cb6df661cbf3e6153057e7576d13b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da336643755ddb0583bdbda4422f3d0b",
+    "url": "/SpotifyManager/index.html"
+  },
+  {
+    "revision": "ae328658c46d55d31958",
+    "url": "/SpotifyManager/static/css/main.ea5a7dad.chunk.css"
+  },
+  {
+    "revision": "e3fd67f8662e17e0914d",
+    "url": "/SpotifyManager/static/js/2.5028d3d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SpotifyManager/static/js/2.5028d3d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae328658c46d55d31958",
+    "url": "/SpotifyManager/static/js/main.0dad648d.chunk.js"
+  },
+  {
+    "revision": "9667fe189ed29355da23",
+    "url": "/SpotifyManager/static/js/runtime-main.bab44874.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spotify-manager/precache-manifest.60bc0d5c167746f23324c6ab6d98898f.js"
+  "/SpotifyManager/precache-manifest.15cb6df661cbf3e6153057e7576d13b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spotify-manager/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SpotifyManager/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
