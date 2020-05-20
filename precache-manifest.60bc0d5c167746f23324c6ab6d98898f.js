88c095b40bcab9f720e98ec64618317cfeebb884
ajgreenwell <greenwan@bc.edu>
2020-05-20 15:23:44 -0400

    Updates

---
+++ precache-manifest.60bc0d5c167746f23324c6ab6d98898f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dadb22f82cc2821dcb601760a37f464",
+    "url": "/spotify-manager/index.html"
+  },
+  {
+    "revision": "41a94e605a99433954cd",
+    "url": "/spotify-manager/static/css/main.ea5a7dad.chunk.css"
+  },
+  {
+    "revision": "e3fd67f8662e17e0914d",
+    "url": "/spotify-manager/static/js/2.5028d3d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spotify-manager/static/js/2.5028d3d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a94e605a99433954cd",
+    "url": "/spotify-manager/static/js/main.37010956.chunk.js"
+  },
+  {
+    "revision": "a1ecb11ac92d8f34ed25",
+    "url": "/spotify-manager/static/js/runtime-main.1f15f9e1.js"
+  }
+]);
